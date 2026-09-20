19875298c37adae52e8db5b2d66756a9afcee944
Shaheen <shn.sdq29@gmail.com>
2026-09-20 22:39:33 +0530

    added ToastContainer for notifications and defined server URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import {Routes,Route} from 'react-router-dom'
+export const serverUrl = "http://localhost:8000"
+import {ToastContainer} from 'react-toastify'
 
 const App = () => {
   return (
     <>
+    <ToastContainer/>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/signup' element={<Signup />} />
