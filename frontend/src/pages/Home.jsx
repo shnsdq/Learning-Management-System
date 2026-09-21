e8461dfe9f7442c983d10c757a5126b08c2b8a3c
Shaheen <shn.sdq29@gmail.com>
2026-09-22 01:16:38 +0530

    updated Home component to include Nav for improved layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Nav from '../component/Nav'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-[100%] overflow-hidden '>
+     <div className='w-[100%] lg:h-[140vh] h-[70vh] relative '>
+      <Nav/>
+     </div>
+    </div>
   )
 }
 
