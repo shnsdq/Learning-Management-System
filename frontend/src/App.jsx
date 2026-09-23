16758970282bed31980be875c54f4d53745a1329
Shaheen <shn.sdq29@gmail.com>
2026-09-23 23:27:03 +0530

    added user authentication checks for Signup, Profile, and ForgetPassword routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, Navigate} from 'react-router-dom'
 export const serverUrl = "http://localhost:8000"
 import {ToastContainer} from 'react-toastify'
 import getCurrentUser from './customHooks/getCurrentUser'
+import { useSelector } from 'react-redux'
+import Profile from './pages/Profile'
+import ForgetPassword from './pages/ForgetPassword'
 
 const App = () => {
   getCurrentUser()
+  const {userData} =useSelector((state)=>state.user)
   return (
     <>
     <ToastContainer/>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/signup' element={<Signup />} />
+      <Route path='/signup' element={!userData ?<Signup /> : <Navigate to={"/"}/>} />
       <Route path='/login' element={<Login />} />
+      <Route path='/profile' element={userData ? <Profile /> : <Navigate to={"/signup"} />} />
+      <Route path='/forget' element={userData ? <ForgetPassword /> : <Navigate to={"/signup"} />} />
     </Routes>
     </>
   )
