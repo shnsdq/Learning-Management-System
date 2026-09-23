0625e5fcff09455e8e4d22fd835a12808a0f51b6
Shaheen <shn.sdq29@gmail.com>
2026-09-23 23:26:28 +0530

    added navigation to password recovery in Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
             {loading ? <ClipLoader size={30} color='white' /> : "Login"}
           </button>
 
-          <span className='text-[13px] cursor-pointer text-[#585757] '>Forget your Passsword ?</span>
+          <span className='text-[13px] cursor-pointer text-[#585757] ' onClick={()=>navigate("/forget")}>Forget your Passsword ?</span>
 
           <div className='w-[80%] flex gap-2 items-center justify-center'>
             <div className='w-[25%] h-[0.5px] bg-[#c4c4c4] '></div>
