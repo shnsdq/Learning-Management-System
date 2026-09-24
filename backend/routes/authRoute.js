8aff198d5abe36509e3ad30f30d906205fb4c21d
Shaheen <shn.sdq29@gmail.com>
2026-09-24 23:58:24 +0530

    added routes for OTP functionality and password reset in authRouter

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, logout, signup } from '../controllers/authController.js'
+import { login, logout, signup,sendOTP,verifyOTP,resetPassword } from '../controllers/authController.js'
 
 
 const authRouter = express.Router()
@@ -7,5 +7,8 @@ const authRouter = express.Router()
 authRouter.post("/signup", signup)
 authRouter.post("/login", login)
 authRouter.get("/logout", logout)
+authRouter.post("/sendotp", sendOTP)
+authRouter.post("/verifyotp", verifyOTP)
+authRouter.post("/resetpassword", resetPassword)
 
 export default authRouter
