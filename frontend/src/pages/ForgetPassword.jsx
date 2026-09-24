e1ccba07bc7e851ff3c50f5e7045bba5ece98868
Shaheen <shn.sdq29@gmail.com>
2026-09-24 23:58:42 +0530

    added OTP sending and verification functionality in ForgetPassword component

--- frontend/src/pages/ForgetPassword.jsx
+++ frontend/src/pages/ForgetPassword.jsx
@@ -1,9 +1,46 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { serverUrl } from '../server'
 
 const ForgetPassword = () => {
     const [step, setStep] = useState(3)
     const navigate = useNavigate()
+    const [email,setEmail] = useState("")
+    const [otp,setOtp] = useState("")
+    const [newPassword,setNewPassword] = useState("")
+    const [conPassword,setConPassword] = useState("")
+    const [loading,setLoading] = useState(false)
+
+   // for step
+   const sendOtp = async () =>{
+        setLoading(true)
+        try {
+            const result = await axios.post(serverUrl +"/api/v1/auth/sendotp",{email},{withCredentials:true}
+            )
+            setLoading(false)
+            setStep(2)
+            toast.success(result.data.message)
+
+   }catch(error){
+        setLoading(false)
+        toast.error(error.response.data.message)
+   } 
+}
+
+//for step 2
+    const verifyOtp = async () => {
+        setLoading(true)
+        try {
+            const result = await axios.post(serverUrl + "/api/v1/auth/verifyotp", { email, otp }, { withCredentials: true })
+            setLoading(false)
+            setStep(3)
+            toast.success(result.data.message)
+        } catch (error) {
+            setLoading(false)
+            toast.error(error.response.data.message)
+        }
+    }
 
     return (
         <div className='min-h-screen flex items-center justify-center bg-gray-100 px-4'>
@@ -13,9 +50,9 @@ const ForgetPassword = () => {
                 <form className='space-y-4' onSubmit={(e) => { e.preventDefault() }}>
                     <div>
                         <label htmlFor="email" className='block text-sm text-gray-700 font-medium'>Enter your Email</label>
-                        <input id='email' type="email" placeholder='Enter your email' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required />
+                        <input id='email' type="email" placeholder='Enter your email' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required onChange={(e) => setEmail(e.target.value)} value={email} />
                     </div>
-                    <button type="submit" className='w-full bg-[black] text-white py-2 px-4 rounded-md hover:bg-[#4b4b4b] font-medium cursor-pointer'>Send OTP</button>
+                    <button className='w-full bg-[black] text-white py-2 px-4 rounded-md hover:bg-[#4b4b4b] font-medium cursor-pointer' disabled={loading} onClick={sendOtp}>{loading ? <Clipboard size={30} color='white' />: "Send OTP"}</button>
                 </form>
                 <div className='text-sm text-center mt-4 cursor-pointer' onClick={()=>navigate("/login")}>Back to Login</div>
 
@@ -27,9 +64,9 @@ const ForgetPassword = () => {
                 <form className='space-y-4' onSubmit={(e) => { e.preventDefault() }}>
                     <div>
                         <label htmlFor="otp" className='block text-sm text-gray-700 font-medium'>Please enter the 4 digit code sent to your email</label>
-                        <input id='otp' type="text" placeholder='* * * *' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required />
+                        <input id='otp' type="text" placeholder='* * * *' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required onChange={(e) => setOtp(e.target.value)} value={otp} />
                     </div>
-                    <button type="submit" className='w-full bg-[black] text-white py-2 px-4 rounded-md hover:bg-[#4b4b4b] font-medium cursor-pointer'>Verify OTP</button>
+                    <button type="submit" className='w-full bg-[black] text-white py-2 px-4 rounded-md hover:bg-[#4b4b4b] font-medium cursor-pointer' onClick={verifyOtp}m>Verify OTP</button>
                 </form>
                 <div className='text-sm text-center mt-4 cursor-pointer' onClick={()=>navigate("/login")}>Back to Login</div>
 
@@ -42,11 +79,11 @@ const ForgetPassword = () => {
                 <form className='space-y-4' onSubmit={(e) => { e.preventDefault() }}>
                     <div>
                         <label htmlFor="password" className='block text-sm text-gray-700 font-medium'>New Password</label>
-                        <input id='password' type="password" placeholder='********' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required />
+                        <input id='password' type="password" placeholder='********' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required onChange={(e) => setNewPassword(e.target.value)} value={newPassword} />
                     </div>
                     <div>
                         <label htmlFor="confirmPassword" className='block text-sm text-gray-700 font-medium'>Confirm Password</label>
-                        <input id='confirmPassword' type="password" placeholder='********' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required />
+                        <input id='confirmPassword' type="password" placeholder='********' className='mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[black]' required onChange={(e) => setConPassword(e.target.value)} value={conPassword} />
                     </div>
                     <button type="submit" className='w-full bg-[black] text-white py-2 px-4 rounded-md hover:bg-[#4b4b4b] font-medium cursor-pointer'>Reset Password</button>
                 </form>
