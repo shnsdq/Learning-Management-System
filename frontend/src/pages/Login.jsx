1f02077bb77e7872610f026a9da8b7c0eaf1de7c
Shaheen <shn.sdq29@gmail.com>
2026-09-25 23:38:50 +0530

    added Google login functionality and improved error handling in Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,6 +8,10 @@ import { ClipLoader } from 'react-spinners';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { setUserData } from '../redux/userSlice';
+import { serverUrl } from '../App';
+import { toast } from 'react-toastify';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../../utils/firebase';
 
 
 const Login = () => {
@@ -22,20 +26,40 @@ const Login = () => {
   const handleLogin = async () => {
     setLoading(true); // Set loading to true when signup is initiated
     try {
-      const result = await axios.post(serverUrl + "/api/auth/login", { password, email }, { withCredentials: true });
+      const result = await axios.post(serverUrl + "/api/auth/login", { email, password }, { withCredentials: true });
+
        dispatch(setUserData(result.data));
       setLoading(false)
       toast.success("Login successful.");
       navigate('/')
 
     } catch (error) {
-      toast.error("error.response.data.message");
+      toast.error(error.response.data.message);
+    }
+  }
+
+  const googleLogin = async () => {
+      try {
+        const response = await signInWithPopup(auth, provider);
+        let user = response.user;
+        let name = user.displayName
+        let email = user.email
+        let role = ""
+        const result = await axios.post(serverUrl + "/api/auth/googleauth", {
+          name,email,role}, { withCredentials: true });
+  
+        dispatch(setUserData(result.data)); // Update Redux store with user data
+        navigate('/')
+        toast.success("Login successful.");
+  
+    }catch (error) {
+  toast.error(response.data.message);
     }
   }
 
   return (
     <div className='bg-[#dddbdb] w-screen h-[100vh] flex justify-center items-center'>
-      <form className='w-[90%] md:w-200 h-150 bg-[white] shadow-xl rounded-2xl flex onSubmit={(e) => { e.preventDefault() }} ' >
+      <form className='w-[90%] md:w-200 h-150 bg-[white] shadow-xl rounded-2xl flex' onSubmit={(e) => { e.preventDefault() }}  >
         {/* left div */}
         <div className='md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3 '>
           <div>
@@ -46,7 +70,7 @@ const Login = () => {
 
           <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3'>
             <label htmlFor="email" className='font-semibold'>Email</label>
-            <input id='email' type="text" placeholder='Your Email' className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px] onChange={(e) => setEmail(e.target.value)} value={email} ' />
+            <input id='email' type="text" placeholder='Your Email' className='border-1 w-[100%] h-[35px] border-[#e7e6e6] text-[15px] px-[20px]' onChange={(e) => setEmail(e.target.value)} value={email}  />
           </div>
 
           <div className='flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative'>
@@ -63,7 +87,7 @@ const Login = () => {
             {loading ? <ClipLoader size={30} color='white' /> : "Login"}
           </button>
 
-          <span className='text-[13px] cursor-pointer text-[#585757] ' onClick={()=>navigate("/forget")}>Forget your Passsword ?</span>
+          <span className='text-[13px] cursor-pointer text-[#585757] ' onClick={()=>navigate('/forget')}>Forget your Passsword ?</span>
 
           <div className='w-[80%] flex gap-2 items-center justify-center'>
             <div className='w-[25%] h-[0.5px] bg-[#c4c4c4] '></div>
@@ -71,7 +95,7 @@ const Login = () => {
             <div className='w-[25%] h-[0.5px] bg-[#c4c4c4] '></div>
           </div>
 
-          <div className='w-[80%] h-[40px] border-1 border-[black] cursor-pointer flex items-center justify-center rounded-[5px] '>
+          <div className='w-[80%] h-[40px] border-1 border-[black] cursor-pointer flex items-center justify-center rounded-[5px] ' onClick={googleLogin} >
             <img src={google} className='w-[25px] ' alt="Google" />
             <span className='text-[18px] text-gray-500 '>oogle</span>
           </div>
