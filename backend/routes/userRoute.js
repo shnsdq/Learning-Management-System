2ef03b74d278bf053a5b9b4f1ff672d6434a1abe
Shaheen <shn.sdq29@gmail.com>
2026-09-26 23:52:14 +0530

    added profile update route with image upload handling

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getCurrentUser } from '../controllers/userController.js'
+import { getCurrentUser, updateProfile } from '../controllers/userController.js'
 import isAuth  from '../middleware/isAuth.js'
+import upload from '../middleware/multer.js'
 
 const userRouter = express.Router()
 
 userRouter.get('/getcurrentuser', isAuth,getCurrentUser)
+userRouter.post('/profile', isAuth,upload.single("photoUrl"),updateProfile)
 
 export default userRouter
