4904ddcddeb1b4260d97687910d94dd231989a0e
Shaheen <shn.sdq29@gmail.com>
2026-09-26 23:52:34 +0530

    added Logos component with icons and descriptions for online courses, access, value, support, and community

--- frontend/src/component/Logos.jsx
+++ frontend/src/component/Logos.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { MdCastForEducation } from "react-icons/md";
+import { SiOpenaccess } from "react-icons/si";
+import { FaSackDollar } from "react-icons/fa6";
+import { BiSupport } from "react-icons/bi";
+import { FaUsers } from "react-icons/fa";
 
 const Logos = () => {
   return (
-    <div>Logos</div>
+    <div className='w-[100vw] min-h[90px] flex items-center justify-center flex-wrap gap-4 md:mb-[50px] '>
+        <div className='flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-22 cursor-pointer text-[03394b] '>
+          <MdCastForEducation className='w-[35px] h-[35px] fill-[#03394b] ' />
+          20k+ Online Courses</div>
+
+        <div className='flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-22 cursor-pointer text-[03394b] '>
+          <SiOpenaccess  className='w-[35px] h-[35px] fill-[#03394b] ' />
+          Lifetime Access</div>
+
+        <div className='flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-22 cursor-pointer text-[03394b] '>
+          <FaSackDollar  className='w-[35px] h-[35px] fill-[#03394b] ' />
+          Value for money</div>
+
+        <div className='flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-22 cursor-pointer text-[03394b] '>
+          <BiSupport className='w-[35px] h-[35px] fill-[#03394b] ' />
+          Lifetime Support</div>
+
+        <div className='flex items-center justify-center gap-2 px-5 py-3 rounded-3xl bg-gray-22 cursor-pointer text-[03394b] '>
+          <FaUsers className='w-[35px] h-[35px] fill-[#03394b] ' />
+          Community Support</div>
+
+    </div>
   )
 }
 
