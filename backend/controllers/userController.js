decd63e1f9d603cca5c97269ce466709d31b6455
Shaheen <shn.sdq29@gmail.com>
2026-09-26 23:51:48 +0530

    added updateProfile function to handle user profile updates with image upload

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from "../config/cloudinary.js";
 import User from "../models/userModel.js";
 
 export const getCurrentUser = async (req, res) => {
@@ -10,5 +11,26 @@ export const getCurrentUser = async (req, res) => {
        return res.status(200).json(user);
     } catch (error) {
        return res.status(500).json({ message: `GetCurrentUser error ${error}`});
+    }
+}
+
+export const updateProfile = async (req,res) =>{
+    try {
+        const userId = req.userId
+        const {description, name} = req.body
+        let photoUrl 
+        if(req.file){
+           photoUrl = await uploadOnCloudinary(req.file.path)
+        }
+        const user = await User.findByIdAndUpdate(userId, {name, description, photoUrl})
+
+        if(!user){
+            return res.status(404).json({ message: 'User not found' });
+        }
+       return res.status(200).json(user);
+
+    } catch (error) {
+          return res.status(500).json({ message: `updateProfile error ${error}`});
+        
     }
 }
